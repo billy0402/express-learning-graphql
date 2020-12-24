5bcd6db8bed542f9dc3eed4e222f61ed9bc80ae7
Billy Huang <10646003@ntub.edu.tw>
2020-12-24 15:40:11 +0800

    feat(mock-server): schema 優先開發

---
+++ mock-server/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer, MockList } = require("apollo-server");
+const { readFileSync } = require("fs");
+
+const typeDefs = readFileSync("./typeDefs.graphql", "UTF-8");
+const resolvers = {};
+
+const mocks = {
+  Query: () => ({
+    totalPhotos: () => 42,
+    allPhotos: () => new MockList([5, 10]),
+    Photo: () => ({ name: "sample photo", description: null }),
+  }),
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  mocks,
+});
+
+server.listen({ port: 4000 }, () => {
+  console.log(
+    `Mock GraphQL Service running @ http://localhost:4000${server.graphqlPath}`
+  );
+});
